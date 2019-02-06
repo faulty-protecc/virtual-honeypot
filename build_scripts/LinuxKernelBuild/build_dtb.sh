d77c9587b97d362804b2ba95b9a1324d949ccbb6
Liron Levy <lironpro14@gmail.com>
2019-02-06 16:52:25 +0200

    now usb fake device exists

---
+++ build_scripts/LinuxKernelBuild/build_dtb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ -d $DIR/../../LinuxKernel/out/dtb ]; then 
+    rm -rf $DIR/../../LinuxKernel/out/dtb
+fi
+mkdir $DIR/../../LinuxKernel/out/dtb
+sudo dtc -o $DIR/../../LinuxKernel/out/dtb/bcm2710-rpi-3-b-plus.dtb $DIR/../../LinuxKernel/dtree_dts/bcm2710-rpi-3-b-plus.dts
