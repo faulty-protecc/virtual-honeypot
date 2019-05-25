d39c8fbe309c6d9948d610fc49726787d02a1ea9
Maor <maor1100@gmail.com>
2019-05-25 16:16:18 +0300

    give the option the limit the number of script runs by parameter

--- build_scripts/access_tester.sh
+++ build_scripts/access_tester.sh
@@ -4,13 +4,25 @@
 # Make sure to clean all files left on the block device in case of logic failure
 trap "rm /mnt/ramdisk_test/testfile* 2>/dev/null; exit" SIGINT SIGTERM
 
+# Parameter number 1 is the number of iterations
+if [ $# -gt 0 ]; then
+	NUMBER_OF_ITERATIONS=$1
+fi
+
 # This function creates temp files on our mount point
 function makeTempFiles
 {
 	mktemp --dry-run -p /mnt/ramdisk_test testfile.XXXXXXXX &
 }
 
-# Make sure that files will be created simulataneously
-while true; do
-	makeTempFiles &
-done
+# Make sure that files will be created simulataneously,
+# if parameter was given it will run the given number
+if [[ $NUMBER_OF_ITERATIONS =~ ^[0-9]+$ ]]; then
+	for index in $(seq 1 $NUMBER_OF_ITERATIONS); do
+		makeTempFiles &
+	done
+else
+	while true; do
+		makeTempFiles &
+	done
+fi
