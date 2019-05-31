cab65e92a011bcdf2ef33d5881f5721d9036d8dc
Liron Levy <lironpro14@gmail.com>
2019-05-31 12:46:05 +0300

    added hypervisor protection activator

---
+++ HypervisorProtectionActivator/activator.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    void* buffer;
+    int fd,hfd;
+    int bytes;
+
+    fd = open("/dev/sbd0",O_RDWR);
+    buffer = mmap(NULL,8192, PROT_READ, MAP_PRIVATE, fd, 0);
+ 
+    hfd  = open("/dev/honeypot", O_RDWR);
+    if (hfd < 0 ){
+            printf("Failed to open ");
+            return -1;
+    }
+    bytes = write(fd, buffer, 8192);
+    if (bytes < 0){
+        perror("failed to write");
+        return -1;
+    }
+    printf("Going to sleep\n");
+    sleep(1);
+    printf("Touching memory, This would trigger the microvisor...\n");
+    /*
+    * Please examine /proc/hyplet_stats for changes
+    */
+   sleep(100);
+}
